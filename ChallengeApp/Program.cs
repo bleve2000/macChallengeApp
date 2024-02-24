32ee7b2251a270de25296fbfc76e6112dd194d1f
tomgier <tigre@wp.pl>
2024-02-24 21:54:01 +0100

    day4 ver.ost

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("It is my first C# programm!");
+﻿// zadanie domowe dzień 4
+
+// zmienne
+var name = "Ewa";
+var age = 22;
+bool woman = true;
+
+// warunki
+if (woman == true && age < 30)
+{ 
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (woman == true && name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (woman == false && name != "Ewa" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni męzczyzna");
+}
